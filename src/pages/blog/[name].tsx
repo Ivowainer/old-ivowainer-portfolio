4376e2b7515ecf5f526187b4afee0282641fae30
Ivowainer <ivancamposwainer@gmail.com>
2022-09-25 19:23:10 -0300

    Getting from another BD POSTS

--- src/pages/blog/[name].tsx
+++ src/pages/blog/[name].tsx
@@ -4,18 +4,18 @@ import getBlog from "../../helpers/getBlog"
 import { blogPropsUnique } from '../../types/BlogTypes';
 
 import MarkdownIt from "markdown-it";
+import emoji from 'markdown-it-emoji'
 
 const NamePost = ({ post }: blogPropsUnique) => {
 
   const md = new MarkdownIt()
+  md.use(emoji)
   const htmlContent = md.render(post.attributes.content)
-
-  console.log(htmlContent)
 
   return (
     <MainLayout pageName={`${post.attributes.Title}`}>
       <div className="px-6 lg:px-20 py-10 flex flex-col gap-3">
-        <h1 className="text-4xl text-gray-200 font-bold">{post.attributes.Title}</h1>
+        <h1 className="text-4xl text-gray-700 dark:text-gray-200 font-bold">{post.attributes.Title}</h1>
         <section className="prose dark:prose-invert lg:prose-xl" dangerouslySetInnerHTML={{__html: htmlContent}}></section>
       </div>
     </MainLayout>
