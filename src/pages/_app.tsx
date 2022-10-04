e38b4e156d585d56ef9a980fad07f52d74001f5b
Ivowainer <ivancamposwainer@gmail.com>
2022-10-04 19:47:18 -0300

    ProgressBar Loading

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 
+import { NextProgressbarSpinner } from 'nextjs-progressbar-spinner'
+
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <ThemeProvider enableSystem={true} attribute="class">
+      <NextProgressbarSpinner
+        NextNProgressProps={{
+          color: 'rgb(16, 185, 129)',
+        }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   ) 
