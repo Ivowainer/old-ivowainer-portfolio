db1cc5ac9c28ef72e60e472872d22f65df3256e0
Ivowainer <ivancamposwainer@gmail.com>
2022-12-29 12:11:29 -0300

    Sitemap w/next

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://ivancamposwainer.com",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
